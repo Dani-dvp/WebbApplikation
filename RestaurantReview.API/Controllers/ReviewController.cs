d282fc40df08cff4e7bd2679d9a364c159b35068
Dani-dvp <kevin1995dani@gmail.com>
2021-05-14 11:25:28 +0200

    Fixed dependencies

---
+++ RestaurantReview.API/Controllers/ReviewController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReview.Application.Features.Reviews.Commands.CreateReview;
+using RestaurantReview.Application.Features.Reviews.Commands.DeleteReview;
+using RestaurantReview.Application.Features.Reviews.Commands.UpdateReview;
+using RestaurantReview.Application.Features.Reviews.Queries.GetReviewListQuery;
+using RestaurantReview.Application.Features.Reviews.Queries.GetReviewsListQuery;
+using ResturantReview.Application.Features.Resturants.Commands.CreateReview;
+using ResturantReview.Application.Features.Reviews.Commands.CreateReview;
+using ResturantReview.Application.Features.Reviews.Commands.DeleteReview;
+using ResturantReview.Application.Features.Reviews.Commands.UpdateReview;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestaurantReview.API.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ICreateReviewService _createReviewService;
+        private readonly IDeleteReviewService _deleteReviewService;
+        private readonly IUpdateReviewService _updateReviewService;
+        private readonly IReviewListQueryService _reviewListQueryService;
+
+        public ReviewController(
+            ICreateReviewService createReviewService,
+            IDeleteReviewService deleteReviewService,
+            IReviewListQueryService reviewListQueryService,
+            IUpdateReviewService updateReviewService)
+
+        {
+            _createReviewService = createReviewService;
+            _deleteReviewService = deleteReviewService;
+            _reviewListQueryService = reviewListQueryService;
+            _updateReviewService = updateReviewService;
+        }
+
+
+        [Authorize]
+        [HttpPost("CreateReview")]
+        public async Task<ActionResult<CreateReviewResponse>> CreateReviewController([FromBody] CreateReviewCommand createReviewCommand)
+        {
+            return Ok(await _createReviewService.CreateReview(createReviewCommand));
+        }
+
+
+        [Authorize]
+        [HttpDelete("DeleteReview")]
+        public async Task<ActionResult<Guid>> DeleteReviewController([FromBody] DeleteReviewCommand deleteReviewCommand)
+        {
+            return Ok(await _deleteReviewService.DeleteReview(deleteReviewCommand));
+        }
+
+
+        [Authorize]
+        [HttpGet("ReviewList")]
+        public async Task<ActionResult<List<ReviewListQueryResponse>>> GetReviewListQueryController()
+        {
+            return Ok(await _reviewListQueryService.GetReviewList());
+        }
+
+
+        [Authorize]
+        [HttpPut("UpdateReview")]
+        public async Task<ActionResult<UpdateReviewResponse>> UpdateReviewController([FromBody] UpdateReviewCommand updateReviewCommand)
+        {
+            return Ok(await _updateReviewService.UpdateReview(updateReviewCommand));
+        }
+
+
+    }
+}
---
+++ RestaurantReview.Authentication/AuthenticationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReview.Domain.AuthenticationModels;
+
+namespace RestaurantReview.Authentication
+{
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
